c633083eab649c9ada21c939b2adfabb7ba9fd50
meliza <s.m.meliza@gmail.com>
2024-02-12 04:38:37 -0500

    Ejercicio 1 y 2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Accordian from './componentes/accordian';
+import RandomColor from './componentes/randon-color';
+
+function App() {
+  return (
+    <div className="App">
+      {/*componente accordion*/}
+      <Accordian/>
+      {/*random color componente*/}
+      <RandomColor/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [ 
+    {
+        id: '1',
+        question: "pregunta 1",
+        answer: "respuesta 1",
+    },
+    {
+        id: '2',
+        question: "pregunta 2",
+        answer: "respuesta 2",
+    },
+    {
+        id: '3',
+        question: "pregunta 3",
+        answer: "respuesta 3",
+    },    
+    {
+        id: '4',
+        question: "pregunta 4",
+        answer: "respuesta 4",
+    },
+]
+
+export default data;
